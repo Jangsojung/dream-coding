f056bd0866a3191d16ed31eb2f30a97db7ed8061
Jangsojung <wkdthwjd99@naver.com>
2024-11-29 17:32:20 +0900

    Refactor: useEffect 사용

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
-  const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
 
   useEffect(() => {
-    fetch('data/products.json') //public/data 접근
+    fetch(`data/${checked ? 'sale_' : ''}products.json`) //public/data 접근
       .then((res) => res.json())
       .then((data) => {
         console.log('데이터 네트워크에서 받아 옴');
@@ -17,21 +21,26 @@ export default function Products() {
     return () => {
       console.log('청소 완료');
     }; // 컴포넌트가 사라질때 호출되는 함수
-  }, []); // 2번째 인자: dependency: [] : 처음 화면에만 호출
+  }, [checked]); // 2번째 인자: dependency: [] : 처음 화면에만 호출
 
   return (
     <>
+      <input id='checkbox' type='checkbox' value={checked} onChange={handleChange} />
+      <label htmlFor='checkbox'>Show Only Sale</label>
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <article>
-              <h3>{product.name}</h3>
-              <p>{product.price}</p>
-            </article>
-          </li>
-        ))}
+        {products.map(
+          (
+            product // list 자식들은 unique key가 있어야 워닝 안 뜸
+          ) => (
+            <li key={product.id}>
+              <article>
+                <h3>{product.name}</h3>
+                <p>{product.price}</p>
+              </article>
+            </li>
+          )
+        )}
       </ul>
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
     </>
   );
 }
