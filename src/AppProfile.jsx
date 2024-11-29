121eacef856d20905c6c460b7d56759fb31617dc
Jangsojung <wkdthwjd99@naver.com>
2024-11-29 16:41:43 +0900

    Feat: event 및 counter

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -3,8 +3,20 @@ import Avatar from './components/Avatar';
 import Profile from './components/Profile';
 
 function AppProfile() {
+  const handleClick = (event) => {
+    console.log(event); // SyntheticBaseEvent: 브라우저에서 발생한 이벤트를 리액트에서 처리할 수 있는 이벤트로 감싸서 전달
+    alert('버튼이 클릭 됨!');
+  };
+
   return (
     <>
+      <button onClick={handleClick}>버튼</button>
+      {/* 함수의 참조값인 이름만 전달해야 handleClick이 참조하는 함수 호출.
+      handleClick()은 함수 호출. 함수 실행 후 반환값이 onClick에 할당 됨. */}
+      <form onSubmit={() => {}}>
+        <input />
+      </form>
+
       <Avatar
         image='https://images.unsplash.com/photo-1731286507379-94216c27cf9d?q=80&w=1512&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         isNew={true}
---
+++ src/components/Counter.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+export default function Counter() {
+  //let num = 0; 안 된다
+  // 변경 가능. 변경 시 화면 업데이트 해줘
+  const [count, setCount] = useState(0); // 상태 값 접근 가능한 변수 value, 업데이트 가능한 function 리턴
+  // count set 될 때 마다 자동으로 Counter() 함수 호출
+  // setCount -> 내부 상태 변경 -> 해당 component 함수 전체 호출
+  // + react는 가상의 dom 요소 사용 -> 변경된 내용만 update
+  // useState 혹은 컴포넌트 내에서 아무리 호출 되어도 값을 기억 -> 0으로 초기화 되지 않음
+
+  return (
+    <div className='counter'>
+      <span className='number'>{count}</span>
+      <button
+        className='button'
+        onClick={() => {
+          // setCount(count + 1);
+          // setCount(count + 1);
+          // setCount(count + 1);
+          // js 클로져 관련.
+          // count는 0이라고 저장 -> 1 그 다음도 0 + 1 = 1 결국 1만 증가
+          setCount((prev) => prev + 1); // prev 0
+          setCount((prev) => prev + 1); // prev 1
+          setCount((prev) => prev + 1); // prev 2
+          //
+        }}
+      >
+        Add +
+      </button>
+    </div>
+  );
+}
